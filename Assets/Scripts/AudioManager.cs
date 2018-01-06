34970b8f10b9518edcd9ff0efc29b8cfd5add442
Unknown <jak3matt@gmail.com>
2018-01-06 01:40:23 -0800

    Created Main Menu
    Added a main menu with all sub menus being navigable. Play does nothing at the moment since there is no main scene.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    [SerializeField] private AudioSource audSource = null;
+    [SerializeField] private List<AudioClip> audioClips = new List<AudioClip>();
+
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Init() {
+        PlayMainMenuMusic();
+    }
+
+    public void PlayMainMenuMusic() {
+        PlaySound(audioClips[0]);
+    }
+
+    public void PlaySound(AudioClip audClip) {
+        audSource.clip = audClip;
+        audSource.Play();
+    }
+}
